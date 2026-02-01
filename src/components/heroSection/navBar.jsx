b5611e8bf0850310a275cbe998b373e769deb90b
kholoud <kholudmahmoud62@gmail.com>
2026-02-01 17:05:00 +0200

    fix ui

--- src/components/heroSection/navBar.jsx
+++ src/components/heroSection/navBar.jsx
@@ -61,15 +61,15 @@ export default function NavBar() {
 
           {/* Mobile Menu Panel */}
           <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pt-2 pb-3">
+            <div className="space-y-1 px-2 pt-2 pb-3 bg-[#FFFF]">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
+                    item.current ? 'bg-gray-900 text-white' : 'text-amber-950 hover:bg-white/5 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium '
                   )}
                 >
                   {item.name}
